ffd4eb819bd5490d22c0503287f4e5ff1fd4e547
unknown <philipkurianv@gmail.com>
2023-08-04 19:40:40 -0400

    Fixed the flickering by using Gltf plane model created in blender, issue with flickering is attributed to react-three-fiber's planeGeometry mapping of textures

--- app/components/SwordModel.tsx
+++ app/components/SwordModel.tsx
@@ -1,6 +1,6 @@
-import { Suspense, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Preload } from '@react-three/drei';
+import { Gltf, OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { TextureLoader } from 'three';
 import { useControls, folder } from 'leva';
 
@@ -9,60 +9,42 @@ interface TweakProps {
 }
 
 function Tweak() {
-  const loader = new TextureLoader();
-  const texture = loader.load('./grid.png');
-  const clouds = loader.load('./clouds.png');
-
-  // const { rotateX, rotateY, rotateZ, color, positionX, positionY, positionZ, scaleX, scaleY, scaleZ } = useControls('PlaneGeometry', {
-  //   transform: folder({
-  //     rotateX: 0.0,
-  //     rotateY: -0.01,
-  //     rotateZ: 3.145,
-  //   }),
-  //   material: folder({
-  //     color: '#333',
-  //     wireframe: true,
-  //   }),
-  //   position: folder({
-  //     positionX: -0.30,
-  //     positionY: -0.66,
-  //     positionZ: -3,
-  //   }),
-  //   scale: folder({
-  //     scaleX: 3,
-  //     scaleY: 3,
-  //     scaleZ: 0.01,
-  //   }),
-  // });
-
+  const plane = useGLTF('./plane.gltf')
+  const meshRef = useRef()  
   const oscillationFrequency = 2 // Adjust the frequency of the oscillation
-  const maxDisplacement = 40 // Adjust the maximum displacement value
-  const smoothingFactor = 0.0002 // Adjust the smoothing factor
-
-  const [targetDisplacementScale, setTargetDisplacementScale] = useState(0);
+  const maxDisplacement = 40 // adjust the maximum displacement value
+  const smoothingFactor = 0.00005 // Adjust the smoothing factor
+  const [targetDisplacementScale, setTargetDisplacementScale] = useState(-3);
   const [currentDisplacementScale, setCurrentDisplacementScale] = useState(0);
 
   useFrame(({ clock }) => {
-    const t = clock.getElapsedTime() * oscillationFrequency;
-    const newDisplacementScale = Math.sin(t) * maxDisplacement;
+    if (meshRef.current) {
+      const t = clock.getElapsedTime() * oscillationFrequency;
+      const newDisplacementScale = Math.sin(t) * maxDisplacement;
 
-    // Smoothly transition between current and new displacement scale
-    const smoothedDisplacementScale = currentDisplacementScale + (newDisplacementScale - currentDisplacementScale) * smoothingFactor;
+      // Smoothly transition between current and new displacement scale
+      const smoothedDisplacementScale = currentDisplacementScale + (newDisplacementScale - currentDisplacementScale) * smoothingFactor;
 
-    setCurrentDisplacementScale(smoothedDisplacementScale);
-    setTargetDisplacementScale(smoothedDisplacementScale);
+      setCurrentDisplacementScale(smoothedDisplacementScale);
+      setTargetDisplacementScale(smoothedDisplacementScale);
+      
+    }
   });
 
   return (
-    <mesh rotation-x={0} rotation-y={-0.01} rotation-z={3.145} position={[-0.30, -0.66, -3]}>
-        {/* <mesh rotation-x={rotateX} rotation-y={rotateY} rotation-z={rotateZ} position={[positionX, positionY, positionZ]} >  */}
-      <planeGeometry  args={[20, 20, 210, 210]} />
+    <mesh 
+      rotation-x={1} 
+      rotation-y={1.577} 
+      rotation-z={39.90} 
+      position={[-0.30, -0.66, targetDisplacementScale -4 ]} 
+    >
+      <primitive ref={meshRef}
+        object={plane.scene} 
+        scale={15} />
 
-      <meshStandardMaterial map={clouds} color={'#333'} displacementMap={clouds} displacementScale={targetDisplacementScale}  />
     </mesh>
   );
 }
-
 
 const ModelCanvas = () => {
   return (
@@ -74,13 +56,13 @@ const ModelCanvas = () => {
       gl={{ preserveDrawingBuffer: true, logarithmicDepthBuffer: true }}
     >
 
-    <hemisphereLight intensity={1.5} position={[4, 4, 1]} groundColor={'#404040'} /> Adjust intensity and groundColor
-      <ambientLight intensity={2} /> {/* Adjust intensity */}
-      {/* <pointLight intensity={2} power={700} position={[0, 0, 0]} /> */}
-      <pointLight intensity={100} power={1000} position={[0, -3, 4]} />
-      <pointLight intensity={100} power={1000} position={[2, 2, -4]} />
+    <hemisphereLight 
+      intensity={1} 
+      position={[4, 4, 1]} 
+      groundColor={'#404040'} /> 
+
+      {/* <ambientLight intensity={1} />  */}
       <Suspense fallback={null}>
-        {/* <OrbitControls enableZoom={false} />    */}
       </Suspense>
       <Preload all />
       <Tweak />
@@ -90,3 +72,25 @@ const ModelCanvas = () => {
 
 
 export default ModelCanvas;
+
+//Paste this function into Tweak() to adjust 3d object with GUI
+  // var { rotateX, rotateY, rotateZ, color, positionX, positionY, scaleX, scaleY, scaleZ } = useControls('PlaneGeometry', {
+  //   transform: folder({
+  //     rotateX: 1,
+  //     rotateY: 1.58,
+  //     rotateZ:39.96,
+  //   }),
+  //   material: folder({
+  //     color: '#333',
+  //     wireframe: true,
+  //   }),
+  //   position: folder({
+  //     positionX: -0.30,
+  //     positionY: -0.66,
+  //   }),
+  //   scale: folder({
+  //     scaleX: 3,
+  //     scaleY: 3,
+  //     scaleZ: 0.01,
+  //   }),
+  // });
