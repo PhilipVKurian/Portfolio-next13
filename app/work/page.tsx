738a67c10f1e52e9c447da1dcb9f3a38bda21540
unknown <philipkurianv@gmail.com>
2023-08-09 09:45:05 -0400

    added some placeholders to work.. needs more styling

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -14,17 +14,45 @@ export default function WorkPage() {
 
         <section className='projects  overflow-y-scroll'>
           
-          <div className='project px-4'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lacus at varius. Integer vel felis at tortor tincidunt feugiat. Vivamus bibendum sapien nec dui aliquet, ac efficitur urna tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Suspendisse potenti. Proin vel justo ut odio ultrices varius. Praesent dapibus dolor a ipsum elementum, et tincidunt neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed fringilla ipsum. Aliquam non semper purus. Donec et justo ac quam congue consectetur non id ligula.</p>
+          <div className='flex lg:flex-row sm:flex-col project px-4 '>
+            <img src="resumaker-form.png" className='w-56 h-40 rounded-lg sm:ml-auto sm:mr-auto' alt="Resumaker"/>
+            <div className='flex flex-col w-[200] mx-2 h-42 overflow-auto border-b-4 border-b-[#888686] bor'>
+              <h4 className='mx-4 opacity-80'>Resumaker</h4>
+              <p className='mx-4 opacity-80'>As Bootcamp Students, we wanted to create an app 
+              that allows users to create free resumes! Trying to get a job is one challenge
+              but having to pay money just to create a resume is NOT COOL! This became our
+              motivation for the project. We want to allow our users to create a 
+              FREE account and not have to enter any credit card details to use the application.</p>
+            </div>
+          </div>
 
-          </div>        
+          <div className='flex lg:flex-row sm:flex-col-reverse project px-4 mt-10'>
+          <div className='flex lg:flex-col sm:flex-col w-[200] mx-2 h-42 overflow-auto border-b-4  border-b-[#888686] '>
+              <h4 className='mx-4 opacity-80'>Resumaker</h4>
+              <p className='mx-4 opacity-80'>As Bootcamp Students, we wanted to create an app 
+              that allows users to create free resumes! Trying to get a job is one challenge
+               but having to pay money just to create a resume is NOT COOL! This became our
+                motivation for the project. We want to allow our users to create a 
+                FREE account and not have to enter any credit card details to use the application.</p>
+            </div>
+            <img src="resumaker-form.png" className='w-56 h-40 rounded-lg sm:ml-auto sm:mr-auto' alt="Resumaker"/>
+
+          </div>
+
+          <div className='flex lg:flex-row sm:flex-col project px-4 mt-10'>
+            <img src="resumaker-form.png" className='w-56 h-40 rounded-lg sm:ml-auto sm:mr-auto' alt="Resumaker"/>
+            <div className='flex flex-col w-[200] mx-2 h-42 overflow-auto border-b-4  border-b-[#888686] '>
+              <h4 className='mx-4 opacity-80'>Resumaker</h4>
+              <p className='mx-4 opacity-80'>As Bootcamp Students, we wanted to create an app 
+              that allows users to create free resumes! Trying to get a job is one challenge
+              but having to pay money just to create a resume is NOT COOL! This became our
+              motivation for the project. We want to allow our users to create a 
+              FREE account and not have to enter any credit card details to use the application.</p>
+            </div>
+          </div>
+          
+             
+
         </section>
 
       </article>
